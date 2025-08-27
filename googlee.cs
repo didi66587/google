8e20fcc81215fe9e725d54466b8953e072443d46
didi66587 <didi999999996@hotmail.com>
2025-08-27 17:35:15 +0300

    Add files via upload

---
+++ googlee.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("google değildir google benzeridir-It is not Google, it is similar to Google.");
+        Console.ReadLine();
+        Console.Write("birşey aratın-search for something");
+        Console.ReadLine();
+    }
+}
